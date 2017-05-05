07afbb3603d09bf5883e5d0e4bbf0e9adaa3b43d
taneeyarele <trele@macalester.edu>
2017-05-04 19:47:18 -0500

    Fixed lose page graphics placement and button sizing

--- Flingshot/js/lose.js
+++ Flingshot/js/lose.js
@@ -1,10 +1,10 @@
 var loseState = {
   create : function(){
     var losePage = game.add.sprite(0,0,'losePage');
-    var resetButton = game.add.sprite(0, 0+50,'resetButton');
+    var resetButton = game.add.sprite(700, 540,'resetButton');
     var finalScore = this.game.state.states['play'].getCurrentScore();
-    var scoreDisplay = game.add.text(650,450, finalScore,{fill: '#7CFC00', fontSize: 130, stroke: '#7CFC00', strokeThickness: 6});
-    resetButton.scale.setTo(0.5,0.5);
+    var scoreDisplay = game.add.text(450,570, finalScore,{fill: '#7CFC00', fontSize: 130, stroke: '#7CFC00', strokeThickness: 6});
+    resetButton.scale.setTo(0.15,0.15);
     resetButton.inputEnabled  = true;
     resetButton.events.onInputDown.add(loseState.restartGame,this);
   },
