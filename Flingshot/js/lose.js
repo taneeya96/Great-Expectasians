144d263e888433870c8d621ce9f1070269a4ab71
Taneeya Rele <trele@macalester.edu>
2018-10-05 13:44:35 -0500

    Screen shake when you lose the game

--- Flingshot/js/lose.js
+++ Flingshot/js/lose.js
@@ -1,6 +1,7 @@
 var loseState = {
   create : function(){
     var losePage = game.add.sprite(0,0,'losePage');
+    this.camera.shake(0.01, 1000, true, Phaser.Camera.SHAKE_BOTH, true);
     var resetButton = game.add.sprite(700, 540,'resetButton');
     var finalScore = this.game.state.states['play'].getCurrentScore();
     var scoreDisplay = game.add.text(430,560, finalScore,{fill: '#7CFC00', fontSize: 130, stroke: '#7CFC00', strokeThickness: 6});
